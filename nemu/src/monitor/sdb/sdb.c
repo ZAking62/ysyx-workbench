8b344dbf0e09a10f0a379fe75f87817911b3fdd8
tracer-ysyx <tracer@ysyx.org>
2023-12-16 11:03:58 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:03:58 up 12 min,  4 users,  load average: 0.00, 0.06, 0.07

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -49,7 +49,7 @@ static int cmd_c(char *args) {
 
 
 static int cmd_q(char *args) {
-  printf("%d", nemu_state.state); 
+  if(nemu_state.state == NEMU_STOP)printf("1");
   nemu_state.state = NEMU_END;
   return -1;
 }
