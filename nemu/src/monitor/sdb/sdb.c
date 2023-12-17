e21154ee4e1b3777056b96ff1fa33a416d965832
tracer-ysyx <tracer@ysyx.org>
2023-12-17 23:16:17 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:16:17 up  8:34,  4 users,  load average: 0.00, 0.02, 0.00

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -79,6 +79,13 @@ static int cmd_info(char *args) {
 
 static int cmd_x(char *args){
 	printf("print memory\n");	
+	char *args_end = args + strlen(args);
+	args = strtok(NULL, " ");
+  if (args == NULL)
+		printf("missing args\n");
+  char *address = args + strlen(args) + 1; //get arguments address
+  if (address >= args_end)
+		printf("error args\n");
 	return 0;
 }
 
