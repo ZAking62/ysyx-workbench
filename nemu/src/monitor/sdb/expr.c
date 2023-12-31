fcda7bb7d68651c74212afca5a506c2cf9ec3ec9
tracer-ysyx <tracer@ysyx.org>
2023-12-31 16:38:14 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:38:14 up  6:44,  3 users,  load average: 0.03, 0.02, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -116,6 +116,7 @@ static bool make_token(char *e) {
 					case TK_EQ:
 						tokens[nr_token].type = rules[i].token_type;
 						strncpy(tokens[nr_token].str, substr_start, substr_len);
+						tokens[nr_token].str[substr_len] = '\0';
 						Log("Load tokens[%d], type = %d, str = %s",
 								nr_token, tokens[nr_token].type, tokens[nr_token].str);
 						nr_token++;
