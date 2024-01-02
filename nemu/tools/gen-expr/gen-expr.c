64442878d0cefcb75c50e14be9cccb4913a543c8
tracer-ysyx <tracer@ysyx.org>
2024-01-03 00:36:13 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:36:13 up 11:00,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -32,21 +32,18 @@ static char *code_format =
 "  return 0; "
 "}";
 
-static void gen_rand_expr() {
-  switch (choose(3)) {
-    case 0: gen_num(); break;
-    case 1: gen('('); gen_rand_expr(); gen(')'); break;
-    default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
-  }
-	buf[0] = '\0';
+uint32_t choose(uint32_t n){
+	uint32_t rand_num = rand() % n;
+	return rand_num;
 }
 
 static void gen(char bracket){
 	buf[nr_buf] = bracket;
+	//printf("write bracket : %c\n", buf[nr_buf]);
 	nr_buf++;	
 }
 
-static void gen_rand_expr(){
+static void gen_rand_op(){
 	char op;
 	switch (choose(4)) {
 		case 0: op = '+'; break;
@@ -55,13 +52,23 @@ static void gen_rand_expr(){
 		default: op = '/'; break;
 	}
 	buf[nr_buf] = op;
+	//printf("write op: %c\n", buf[nr_buf]);
 	nr_buf++;
 }
 
 static void gen_num(){
-	word_t rand_num = choose(0x100000000);
-	int len = sprintf(buf, "%u", rand_num);
+	uint32_t rand_num = choose(0xff) + 1;
+	int len = sprintf(buf + nr_buf, "%u", rand_num);
+	//printf("write : %s\n", buf + nr_buf);
 	nr_buf += len;
+}
+
+static void gen_rand_expr() {
+  switch (choose(3)) {
+    case 0: gen_num(); break;
+    case 1: gen('('); gen_rand_expr(); gen(')'); break;
+    default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -73,27 +80,39 @@ int main(int argc, char *argv[]) {
   }
   int i;
   for (i = 0; i < loop; i ++) {
-		//need initialize
+		memset(buf, 0, sizeof(buf));
+		nr_buf = 0;
+
     gen_rand_expr();
+		buf[nr_buf] = '\0';
 
     sprintf(code_buf, code_format, buf);
 
+		//write code_buf into /tmp/.code.c
     FILE *fp = fopen("/tmp/.code.c", "w");
     assert(fp != NULL);
     fputs(code_buf, fp);
     fclose(fp);
 
-    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
+		//call shell command gcc compile, -w ignore warning
+		//linux return 0 when shell command executes successfully
+    int ret = system("gcc -w /tmp/.code.c -o /tmp/.expr");
     if (ret != 0) continue;
 
+		//execute command /tmp/.expr, read the command's stdout
     fp = popen("/tmp/.expr", "r");
     assert(fp != NULL);
 
+		//load stdout into result
     int result;
     ret = fscanf(fp, "%d", &result);
     pclose(fp);
+		if(ret != 1){
+			i--;
+			continue;
+		}
 
-    printf("%u %s\n", result, buf);
+    printf("result = %u, buf = %s\n", result, buf);
   }
   return 0;
 }
