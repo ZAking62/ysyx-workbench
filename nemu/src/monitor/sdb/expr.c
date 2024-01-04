45b63256468fe68674fcba348622b3bd1e181c4d
tracer-ysyx <tracer@ysyx.org>
2024-01-04 23:11:22 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:11:22 up  6:07,  3 users,  load average: 0.08, 0.04, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -301,7 +301,7 @@ void test_expr() {
 	char *is_read = fgets(buf, 65536 + 128, fp);
 	while(is_read != NULL){
 		printf("buf = %s", buf);
-		sscanf(buf, "%d %s\n", &result, temp_expr);
+		sscanf(buf, "%s\n", temp_expr);
 		printf("result = %d, expr = %s\n", result, temp_expr);
 		word_t ans = expr(temp_expr, &success);
 		if(ans == result){
