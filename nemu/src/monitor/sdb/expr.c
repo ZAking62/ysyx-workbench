d8f8b47d6c65b924cb2dcb63e0dd8753487f7d99
tracer-ysyx <tracer@ysyx.org>
2024-01-04 23:40:44 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:40:44 up  6:36,  4 users,  load average: 0.07, 0.04, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -201,8 +201,8 @@ int prior_level(int type){
 	switch (type) {
     case '+':
 		case '-': return 1;
-    case '*': return 2;
-    case '/': return 3;
+    case '*': 
+    case '/': return 2;
 		default: assert(0);
 	}
 }
@@ -237,7 +237,7 @@ word_t eval(int p, int q, bool *success) {
   }
   else {
 		int op = p;
-		int op_type = '/'; 
+		int op_type = '*'; 
 		int in_bracket = 0;
 		word_t val1, val2;
 		for(int i = p; i <= q; i++){
