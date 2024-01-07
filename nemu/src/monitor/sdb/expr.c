736c0e00206a3680e0ab999767acd27c793164b0
tracer-ysyx <tracer@ysyx.org>
2024-01-08 00:16:58 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:16:58 up  2:23,  3 users,  load average: 0.11, 0.04, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -330,7 +330,7 @@ uint32_t expr(char *e, bool *success) {
 	  if (tokens[i].type == '*' && (i == 0 || 
 					(tokens[i - 1].type != TK_NUM && tokens[i - 1].type != ')'))) {
 	    tokens[i].type = DEREF;
-			printf("position %d modify to DEREF\n", i);
+			//printf("position %d modify to DEREF\n", i);
 	  }
 	}
 
