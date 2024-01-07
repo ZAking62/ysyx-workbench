d003d9d5f89c51ad0286aefb5e2e6d0c1fc6b48e
tracer-ysyx <tracer@ysyx.org>
2024-01-08 00:11:51 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:11:51 up  2:18,  3 users,  load average: 0.12, 0.04, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -295,7 +295,7 @@ uint32_t eval(int p, int q, bool *success) {
    //op = the position of 主运算符 in the token expression;
 		val2 = eval(op + 1, q, success);
     if(op_type == DEREF){
-			return *(uint32_t*)guest_to_host(val2);		
+			return paddr_read(val2, 1);		
 		}
 		val1 = eval(p, op - 1, success);
 
