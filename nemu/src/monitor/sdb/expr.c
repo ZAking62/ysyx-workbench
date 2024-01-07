0f08a048813851ef80c2b3f50431429085abcab6
tracer-ysyx <tracer@ysyx.org>
2024-01-07 11:24:13 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:24:13 up 32 min,  3 users,  load average: 0.19, 0.09, 0.03

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -260,7 +260,7 @@ uint32_t eval(int p, int q, bool *success) {
   }
   else {
 		int op = p;
-		int op_type = 280; 
+		int op_type = '*'; 
 		int in_bracket = 0;
 		uint32_t val1, val2;
 		for(int i = p; i <= q; i++){
