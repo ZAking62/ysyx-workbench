f261238572cda0fe78723c8d695aacd3baae88e5
tracer-ysyx <tracer@ysyx.org>
2024-01-09 23:16:28 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:16:28 up  1:33,  4 users,  load average: 0.00, 0.03, 0.00

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -123,6 +123,7 @@ static int cmd_w(char *args){
 		return 0;
 	}
 	WP *wpoint = new_wp();
+	printf("dd\n");
 	strcpy(wpoint->expr, args);
 	bool success = true;
 	wpoint->value = expr(args, &success);
