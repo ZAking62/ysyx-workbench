6066152c3975101de4334460e659bf0ea60171f6
tracer-ysyx <tracer@ysyx.org>
2024-01-11 00:10:49 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:10:49 up  3:49,  4 users,  load average: 0.01, 0.02, 0.00

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -94,11 +94,13 @@ bool wp_change(){
 	bool res = false;
 	bool success = true;
 	word_t pre_value = 0;
+	char *tem_expr = NULL;
 	WP* cur = head;
 	while(cur != NULL){
 		success = true;
 		pre_value = cur->value;
-		cur->value = expr(cur->expr, &success);
+		strcpy(tem_expr, cur->expr);
+		cur->value = expr(tem_expr, &success);
 		if(cur->value != pre_value && success == true){
 			printf("watchpoint id = %d, expr = %s, value = 0x%x, %u change to 0x%x, %u\n", 
 				cur->NO, cur->expr, pre_value, pre_value, cur->value, cur->value);
