a6a8f0f9070b63d21588d6be4ffb5bbff206d0ea
tracer-ysyx <tracer@ysyx.org>
2024-01-26 12:20:55 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:20:55 up  1:01,  4 users,  load average: 0.09, 0.03, 0.01

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -41,6 +41,9 @@ gdb: run-env
 	$(call git_commit, "gdb NEMU")
 	gdb -s $(BINARY) --args $(NEMU_EXEC)
 
+count:
+	find $(NEMU_HOME) -name "*.[ch]" | xargs grep -v "^$$" | wc -l
+
 clean-tools = $(dir $(shell find ./tools -maxdepth 2 -mindepth 2 -name "Makefile"))
 $(clean-tools):
 	-@$(MAKE) -s -C $@ clean
