c5f09db7db3e094c0a43f224c5a56b1d9357196d
tracer-ysyx <tracer@ysyx.org>
2024-02-17 23:45:50 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:45:50 up  7:01,  3 users,  load average: 0.16, 0.06, 0.01

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,25 +4,89 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+static char NUM_CHAR[] = "0123456789ABCDEF";
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+	char buffer[2048];
+	va_list arg;
+	va_start(arg, fmt);
+
+	int done = vsprintf(buffer, fmt, arg);
+	putstr(buffer);
+
+	va_end(arg);
+	return done;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+	return vsnprintf(out, -1, fmt, ap);
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+	va_list arg;
+	va_start(arg, fmt);
+
+	int done = vsprintf(out, fmt, arg);
+
+	va_end(arg);
+	return done;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+	va_list arg;
+	va_start(arg, fmt);
+
+	int done = vsnprintf(out, n, fmt, arg);
+
+	va_end(arg);
+	return done;
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+	int len = 0;
+	char buf[128];
+	int buf_len = 0;
+	while(*fmt != '\0' && len < n){
+		switch(*fmt){
+			case '%':
+				fmt++;
+				switch(*fmt){
+					case 'd':
+						int val = va_arg(ap, int);
+						if(val == 0){
+							out[len++] = '0';
+							break;
+						}
+						if(val < 0){
+							out[len++] = '-';
+							val = -val;
+						}
+						while(val != 0){
+							buf[buf_len++] = NUM_CHAR[val % 10];
+							val /= 10;
+						}
+						while(buf_len != 0){
+							out[len++] = buf[--buf_len];
+						}
+						break;
+					case 's':
+						char *s = va_arg(ap, char*);
+            for(int i = 0; s[i] != '\0'; i++){
+                out[len++] = s[i];
+						}
+						break;
+				}
+				break;
+			case '\n':
+				out[len++] = '\n';
+				break;
+			default:
+				out[len++] = *fmt;
+		}
+		fmt++;
+	}
+	out[len] = '\0';
+	return len;
 }
 
 #endif
