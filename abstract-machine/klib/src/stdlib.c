30e66dc86657428bff3e06361110fac4f36c08ee
tracer-ysyx <tracer@ysyx.org>
2024-03-04 17:25:16 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:25:16 up  4:21,  4 users,  load average: 0.00, 0.03, 0.00

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -29,21 +29,20 @@ int atoi(const char* nptr) {
   return x;
 }
 
-//static char *m_addr;
-//static int init_flag = 0;
+static char *m_addr;
+static int init_flag = 0;
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-//	if(!init_flag){
-//		m_addr = (void*)ROUNDUP(heap.start, 8);
-//		init_flag = 1;
-//	}
-//  size = (size_t)ROUNDUP(size, 8);
-//  char *old = m_addr;
-		char *old = (void*)heap.start;
-//  m_addr += size;
+	if(!init_flag){
+		m_addr = (void*)(heap.start);
+		init_flag = 1;
+	}
+  size = (size_t)ROUNDUP(size, 8);
+  char *old = m_addr;
+  m_addr += size;
   return old;
 #endif
   return NULL;
