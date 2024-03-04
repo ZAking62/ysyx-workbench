70f3fcde188061e79e44e2e5b32dcfcf0dab7fa7
tracer-ysyx <tracer@ysyx.org>
2024-03-04 17:30:56 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:30:56 up  4:27,  4 users,  load average: 0.00, 0.01, 0.00

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -29,25 +29,37 @@ int atoi(const char* nptr) {
   return x;
 }
 
-static char *m_addr;
-static int init_flag = 0;
-void *malloc(size_t size) {
-  // On native, malloc() will be called during initializaion of C runtime.
-  // Therefore do not call panic() here, else it will yield a dead recursion:
-  //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-	if(!init_flag){
-		m_addr = (void*)heap.start;
-		init_flag = 1;
-	}
-  size = (size_t)ROUNDUP(size, 8);
-  char *old = m_addr;
-  m_addr += size;
-  return old;
-#endif
-  return NULL;
-}
+//static char *m_addr;
+//static int init_flag = 0;
+//void *malloc(size_t size) {
+//  // On native, malloc() will be called during initializaion of C runtime.
+//  // Therefore do not call panic() here, else it will yield a dead recursion:
+//  //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
+//#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+//	if(!init_flag){
+//		m_addr = (void*)ROUNDUP(heap.start, 8);
+//		init_flag = 1;
+//	}
+//  size = (size_t)ROUNDUP(size, 8);
+//  char *old = m_addr;
+//  m_addr += size;
+//  return old;
+//#endif
+//  return NULL;
+//}
+static char* start_addr;    // addr的初始值
+static bool init_flag = 0;  // 初始化的标志, 初始化完成后置1
 
+void *malloc(size_t size) {
+    if(!init_flag) {
+        start_addr = (void*)ROUNDUP(heap.start, 8);
+        init_flag = true;
+    }
+    size = (size_t)ROUNDUP(size, 8);
+    char* old = start_addr; // 获取addr
+    start_addr += size;
+    return old; // [addr, addr + size]
+}
 void free(void *ptr) {
 }
 
