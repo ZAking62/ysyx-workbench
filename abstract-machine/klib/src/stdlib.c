a6747d01a4d409f6a505e291490464894cb15566
tracer-ysyx <tracer@ysyx.org>
2024-03-04 17:18:15 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:18:15 up  4:14,  4 users,  load average: 0.08, 0.06, 0.01

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -5,7 +5,7 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
 static char *m_addr;
-static int init_flag = 0;
+//static int init_flag = 0;
 
 int rand(void) {
   // RAND_MAX assumed to be 32767
@@ -36,17 +36,16 @@ void *malloc(size_t size) {
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-	if(!init_flag){
-		m_addr = (void*)ROUNDUP(heap.start, 8);
-		init_flag = 1;
-	}
+//	if(!init_flag){
+//		m_addr = (void*)ROUNDUP(heap.start, 8);
+//		init_flag = 1;
+//	}
   size = (size_t)ROUNDUP(size, 8);
   char *old = m_addr;
   m_addr += size;
   return old;
 #endif
   return NULL;
-	panic("dd");
 }
 
 void free(void *ptr) {
