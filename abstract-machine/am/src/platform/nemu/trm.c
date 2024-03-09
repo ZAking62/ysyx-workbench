0546b3f690267a4f4040ce96ad0f342f045b337f
tracer-ysyx <tracer@ysyx.org>
2024-03-09 23:48:26 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:48:26 up  2:14,  4 users,  load average: 0.01, 0.02, 0.00

--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -4,10 +4,6 @@
 extern char _heap_start;
 int main(const char *args);
 
-//abstract-machine/am/include/am.h列出了AM中的所有API
-//abstract-machine/am/ 不同架构的AM API实现, 目前只需要关注NEMU相关的内容.
-//每个架构都按照它们的特性实现这组API; 应用程序只需要直接调用这组API即可
-//(在NEMU中)实现硬件功能 -> (在AM中)提供运行时环境 -> (在APP层)运行程序
 //Area heap结构用于指示堆区的起始和末尾
 Area heap = RANGE(&_heap_start, PMEM_END);
 #ifndef MAINARGS
