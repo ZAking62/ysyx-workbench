937cb700d6d82bff48480274494ae45834e37eb6
tracer-ysyx <tracer@ysyx.org>
2024-03-10 19:42:20 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:42:20 up 53 min,  3 users,  load average: 0.01, 0.02, 0.00

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -21,8 +21,14 @@
 #include <utils.h>
 #include <difftest-def.h>
 
+void difftest_regcpy(void *dut, bool direction);
+// 在DUT host memory的`buf`和REF guest memory的`addr`之间拷贝`n`字节,
+// `direction`指定拷贝的方向, `DIFFTEST_TO_DUT`表示往DUT拷贝, `DIFFTEST_TO_REF`表示往REF拷贝
 void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
+// `direction`为`DIFFTEST_TO_DUT`时, 获取REF的寄存器状态到`dut`;
+// `direction`为`DIFFTEST_TO_REF`时, 设置REF的寄存器状态为`dut`;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
+// 让REF执行`n`条指令
 void (*ref_difftest_exec)(uint64_t n) = NULL;
 void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
 
@@ -33,6 +39,7 @@ static int skip_dut_nr_inst = 0;
 
 // this is used to let ref skip instructions which
 // can not produce consistent behavior with NEMU
+// 通过difftest_skip_ref()进行校准, 执行它后, 在difftest_step()中会让REF跳过当前指令的执行, 同时把NEMU的当前的寄存器状态直接同步到REF中, 效果相当于"该指令的执行结果以NEMU的状态为准"
 void difftest_skip_ref() {
   is_skip_ref = true;
   // If such an instruction is one of the instruction packing in QEMU
@@ -61,11 +68,12 @@ void difftest_skip_dut(int nr_ref, int nr_dut) {
 
 void init_difftest(char *ref_so_file, long img_size, int port) {
   assert(ref_so_file != NULL);
-
+//打开传入的动态库文件ref_so_file.
   void *handle;
   handle = dlopen(ref_so_file, RTLD_LAZY);
   assert(handle);
 
+//通过动态链接对动态库中的上述API符号进行符号解析和重定位, 返回它们的地址.
   ref_difftest_memcpy = dlsym(handle, "difftest_memcpy");
   assert(ref_difftest_memcpy);
 
@@ -86,13 +94,18 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
       "This will help you a lot for debugging, but also significantly reduce the performance. "
       "If it is not necessary, you can turn it off in menuconfig.", ref_so_file);
 
+//对REF的DIffTest功能进行初始化, 具体行为因REF而异.
   ref_difftest_init(port);
+//将DUT的guest memory拷贝到REF中.
   ref_difftest_memcpy(RESET_VECTOR, guest_to_host(RESET_VECTOR), img_size, DIFFTEST_TO_REF);
+//将DUT的寄存器状态拷贝到REF中.
   ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
 }
 
+//不同isa实现抽象成checkregs
 static void checkregs(CPU_state *ref, vaddr_t pc) {
   if (!isa_difftest_checkregs(ref, pc)) {
+		//具体isa实现在nemu/src/isa/riscv32/difftest/dut.c
     nemu_state.state = NEMU_ABORT;
     nemu_state.halt_pc = pc;
     isa_reg_display();
@@ -122,9 +135,11 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
     return;
   }
 
+//在NEMU中执行完一条指令后, 就在difftest_step()中让REF执行相同的指令
   ref_difftest_exec(1);
+//读出REF中的寄存器
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
-
+//并进行对比
   checkregs(&ref_r, pc);
 }
 #else
