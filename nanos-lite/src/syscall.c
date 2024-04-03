f361efc01e2c7fbd5e34d8262e6bd8d7e76c0d72
tracer-ysyx <tracer@ysyx.org>
2024-04-03 17:56:39 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:56:39 up  3:59,  4 users,  load average: 0.25, 0.12, 0.10

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,29 @@
 #include <common.h>
 #include "syscall.h"
 
+enum {
+  SYS_exit,
+  SYS_yield,
+  SYS_open,
+  SYS_read,
+  SYS_write,
+  SYS_kill,
+  SYS_getpid,
+  SYS_close,
+  SYS_lseek,
+  SYS_brk,
+  SYS_fstat,
+  SYS_time,
+  SYS_signal,
+  SYS_execve,
+  SYS_fork,
+  SYS_link,
+  SYS_unlink,
+  SYS_wait,
+  SYS_times,
+  SYS_gettimeofday
+};
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1; //a7
@@ -8,14 +31,14 @@ void do_syscall(Context *c) {
 
 	//参考应用的系统调用号
   switch (a[0]) {
-    case 0:
+    case SYS_exit:
 			Log("SYS_exit, GPRx = %d", c->GPRx);
 			//c->GPRx=0;
 			halt(c->GPRx);//SYS_exit系统调用
-    case 1:
+    case SYS_yield:
 			Log("SYS_yield, GPRx = %d", c->GPRx);
 			yield(); //SYS_yield系统调用
-		case 4:
+		case SYS_write:
 			Log("SYS_write, GPRx = %d", c->GPRx);
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
