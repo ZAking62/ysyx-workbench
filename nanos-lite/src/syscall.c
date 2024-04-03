173698601d8f8384ba17862f15bea68a9523c5da
tracer-ysyx <tracer@ysyx.org>
2024-04-03 11:00:01 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:00:01 up 26 min,  4 users,  load average: 0.03, 0.01, 0.00

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -7,7 +7,10 @@ void do_syscall(Context *c) {
 
 	//参考应用的系统调用号
   switch (a[0]) {
-    case 0:c->GPRx=0;printf("SYS_exit， do_syscall此时 c->GPRx=%d\n",c->GPRx);halt(c->GPRx);//SYS_exit系统调用
+    case 0:
+			Log("SYS_exit");
+			c->GPRx=0;
+			halt(0);//SYS_exit系统调用
     case 1:printf("SYS_yield， do_syscall此时c->GPRx=%d\n",c->GPRx);yield(); //SYS_yield系统调用
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
