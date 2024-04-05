d36a6ebbafbe3239bd72256736c22684d3087990
tracer-ysyx <tracer@ysyx.org>
2024-04-05 09:46:39 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:46:39 up 42 min,  1 user,  load average: 0.16, 0.18, 0.12

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -35,11 +35,11 @@ void sys_yield(Context *c){
 }
 
 void sys_write(Context *c){
- // assert(c->GPR2 == 1 || c->GPR2 == 2);
+  assert(c->GPR2 == 1 || c->GPR2 == 2);
   for (int i = 0; i < c->GPR4; ++i){
 		putch(*(((char *)c->GPR3) + i));
   }
-  c->GPRx = c->GPR4;
+  //c->GPRx = c->GPR4;
 }
 
 void sys_brk(Context *c){
