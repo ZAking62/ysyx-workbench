4ec4c97606e442cb4d4210f6070d1b8c0dc7942c
tracer-ysyx <tracer@ysyx.org>
2024-04-05 20:29:12 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:29:12 up 50 min,  1 user,  load average: 0.00, 0.03, 0.00

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -67,8 +67,8 @@ int _open(const char *path, int flags, mode_t mode) {
 
 int _write(int fd, void *buf, size_t count) {
   assert(fd == 1 || fd == 2);
-	_syscall_(SYS_write, fd, (intptr_t)buf, count);
-  return -1;
+	int res = _syscall_(SYS_write, fd, (intptr_t)buf, count);
+  return res;
 }
 
 
