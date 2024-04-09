cbf4379c6d30ba59c8cb118c413fc1980b3c68a8
tracer-ysyx <tracer@ysyx.org>
2024-04-09 21:05:55 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:05:55 up  7:47,  3 users,  load average: 0.22, 0.09, 0.03

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,7 +27,7 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
-	naive_uload(NULL, "navy-apps/build/ramdisk.img");
+	naive_uload(NULL, "/navy-apps/build/ramdisk.img");
 }
 
 Context* schedule(Context *prev) {
