8b3e1fc3ea959d36eedd154af6bbb1250e2b89fb
tracer-ysyx <tracer@ysyx.org>
2024-04-11 18:25:10 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:25:10 up  2:56,  3 users,  load average: 0.11, 0.10, 0.03

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -5,11 +5,10 @@ int main() {
   write(1, "Hello World!\n", 13);
   int i = 2;
   volatile int j = 0;
-  while (1) {
+  while (j <= 100000) {
     j ++;
-    if (j == 10000) {
+    if (j % 10000 == 0) {
       printf("Hello World from Navy-apps for the %dth time!\n", i ++);
-      j = 0;
     }
   }
   return 0;
