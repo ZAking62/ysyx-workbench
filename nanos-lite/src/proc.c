b5b4b8b26e5c0ef35ce9807cbc3bb2b7b9d369c3
tracer-ysyx <tracer@ysyx.org>
2024-04-14 21:00:56 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:00:56 up  4:45,  3 users,  load average: 0.10, 0.14, 0.10

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -28,7 +28,7 @@ void init_proc() {
 
   // load program here
   //只需要修改传入naive_uload()函数的文件名即可
-	naive_uload(NULL, "/bin/timer-test");
+	naive_uload(NULL, "/bin/bmp-test");
 }
 
 Context* schedule(Context *prev) {
