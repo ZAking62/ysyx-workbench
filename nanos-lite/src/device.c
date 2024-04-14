a92e55e6472dab312240f5af61435aeea7144f19
tracer-ysyx <tracer@ysyx.org>
2024-04-14 23:32:57 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:32:57 up  1:24,  3 users,  load average: 0.21, 0.18, 0.09

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -31,7 +31,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   AM_GPU_CONFIG_T ev = io_read(AM_GPU_CONFIG);
   Log("width = %d, height = %d", ev.width, ev.height);
-  return sprintf((char *)buf, "WIDTH:%d\nHEIGHT:%d\n", ev.width, ev.height);
+  return snprintf((char *)buf, len, "WIDTH:%d\nHEIGHT:%d\n", ev.width, ev.height);
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
