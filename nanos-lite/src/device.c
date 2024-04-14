a03781f947d0c17558ae97d0621c154996be2ed6
tracer-ysyx <tracer@ysyx.org>
2024-04-14 19:36:11 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:36:11 up  3:20,  3 users,  load average: 0.27, 0.12, 0.04

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -28,45 +28,45 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  // AM_GPU_CONFIG_T t = io_read(AM_GPU_CONFIG);
-  // return snprintf((char *)buf, len, "WIDTH:%d\nHEIGHT:%d\n", t.width, t.height);
-  if (offset > 0){
-    return 0;
-  }
+  AM_GPU_CONFIG_T t = io_read(AM_GPU_CONFIG);
+  return snprintf((char *)buf, len, "WIDTH:%d\nHEIGHT:%d\n", t.width, t.height);
+  // if (offset > 0){
+  //   return 0;
+  // }
 
-  int w = io_read(AM_GPU_CONFIG).width;
-  int h = io_read(AM_GPU_CONFIG).height;
+  // int w = io_read(AM_GPU_CONFIG).width;
+  // int h = io_read(AM_GPU_CONFIG).height;
 
-  int ret = snprintf(buf, len, "WIDTH:%d\nHEIGHT:%d", w, h);
-  Log("%s", (char *)buf);
-  if (ret >= len){
-    assert(0);
-  }
+  // int ret = snprintf(buf, len, "WIDTH:%d\nHEIGHT:%d", w, h);
+  // Log("%s", (char *)buf);
+  // if (ret >= len){
+  //   assert(0);
+  // }
 
-  return ret + 1;
+  // return ret + 1;
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
 
-  // AM_GPU_CONFIG_T ev = io_read(AM_GPU_CONFIG);
-  // int width = ev.width;
+  AM_GPU_CONFIG_T ev = io_read(AM_GPU_CONFIG);
+  int width = ev.width;
  
-  // offset /= 4;
-  // len /= 4;
+  offset /= 4;
+  len /= 4;
  
-  // int y = offset / width;
-  // int x = offset - y * width;
+  int y = offset / width;
+  int x = offset - y * width;
  
-  // io_write(AM_GPU_FBDRAW, x, y, (void *)buf, len, 1, true);
+  io_write(AM_GPU_FBDRAW, x, y, (void *)buf, len, 1, true);
  
-  // return len;
-  uintptr_t *ptr;
-  ptr = (uintptr_t *)(&buf);
-
-  io_write(AM_GPU_MEMCPY, offset, (void *)*ptr, len);
-  io_write(AM_GPU_FBDRAW, 0, 0, NULL, 0, 0, true);
-  
   return len;
+  // uintptr_t *ptr;
+  // ptr = (uintptr_t *)(&buf);
+
+  // io_write(AM_GPU_MEMCPY, offset, (void *)*ptr, len);
+  // io_write(AM_GPU_FBDRAW, 0, 0, NULL, 0, 0, true);
+  
+  // return len;
 }
 
 void init_device() {
