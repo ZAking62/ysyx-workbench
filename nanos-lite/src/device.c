d0bdab1707983a1480d7475c4241b4a20fa33780
tracer-ysyx <tracer@ysyx.org>
2024-04-14 21:09:06 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:09:06 up  4:53,  3 users,  load average: 0.28, 0.28, 0.19

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -33,23 +33,16 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  // AM_GPU_CONFIG_T ev = io_read(AM_GPU_CONFIG);
-  // int width = ev.width;
-  // offset /= 4;
-  // len /= 4;
+  AM_GPU_CONFIG_T ev = io_read(AM_GPU_CONFIG);
+  int width = ev.width;
+  offset /= 4;
+  len /= 4;
  
-  // int y = offset / width;
-  // int x = offset - y * width;
+  int y = offset / width;
+  int x = offset - y * width;
  
-  // io_write(AM_GPU_FBDRAW, x, y, (void *)buf, len, 1, true);
+  io_write(AM_GPU_FBDRAW, x, y, (void *)buf, len, 1, true);
  
-  // return len;
-  uintptr_t *ptr;
-  ptr = (uintptr_t *)(&buf);
-
-  io_write(AM_GPU_MEMCPY, offset, (void *)*ptr, len);
-  io_write(AM_GPU_FBDRAW, 0, 0, NULL, 0, 0, true);
-  
   return len;
 }
 
--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -1,255 +1,45 @@
-// #include <stdint.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include <unistd.h>
-// #include <sys/time.h>
-// #include <assert.h>
-// #include <fcntl.h>
-
-// static int evtdev = -1;
-// static int fbdev = -1;
-// static int screen_w = 0, screen_h = 0;
-// //画布大小
-// static int canvas_w = 0, canvas_h = 0;
-// //相对于屏幕左上角的画布位置坐标
-// static int canvas_x = 0, canvas_y = 0;
-
-// uint32_t NDL_GetTicks() {
-//   struct timeval tv;
-//   gettimeofday(&tv, NULL);
-//   return tv.tv_usec / 1000 + tv.tv_sec * 1000;
-// }
-
-// // 读出一条事件信息到buf中, 最长len字节
-// int NDL_PollEvent(char *buf, int len) {
-//   int fd = open("/dev/events", 0, 0);
-//   int ret = read(fd, buf, len);
-//   close(fd);
-//   return ret == 0 ? 0 : 1;
-// }
-
-// // 打开一张(*w) X (*h)的画布
-// // 如果*w和*h均为0, 则将系统全屏幕作为画布, 并将*w和*h分别设为系统屏幕的大小
-// void NDL_OpenCanvas(int *w, int *h) {
-// //   int buf_size = 1024; 
-// //   char * buf = (char *)malloc(buf_size * sizeof(char));
-// //   int fd = open("/proc/dispinfo", 0, 0);
-// //   int ret = read(fd, buf, buf_size);
-// //   assert(ret < buf_size); // to be cautious...
-// //   assert(close(fd) == 0);
- 
-// //   int i = 0;
-// //   int width = 0, height = 0;
-// // //使用 strncmp 函数检查字符串 "WIDTH" 是否位于 buf 中 i 处开始的位置，以确保文件内容的格式正确。
-// //   assert(strncmp(buf + i, "WIDTH", 5) == 0);
-// //   //这一行将 i 增加 5，以跳过字符串 "WIDTH"。
-// //   i += 5;
-// //   for (; i < buf_size; ++i) {
-// //       if (buf[i] == ':') { i++; break; }
-// //       assert(buf[i] == ' ');
-// //   }
-// //   for (; i < buf_size; ++i) {
-// //     //检查当前字符是否是数字字符。如果是，它跳出循环以开始解析宽度值。
-// //       if (buf[i] >= '0' && buf[i] <= '9') break;
-// //       assert(buf[i] == ' ');
-// //   }
-// //   for (; i < buf_size; ++i) {
-    
-// //       if (buf[i] >= '0' && buf[i] <= '9') {
-// //         //检查当前字符是否是数字字符。如果是，它将当前字符的数字值添加到 width 变量中。
-// //           width = width * 10 + buf[i] - '0';
-// //       } else {
-// //           break;
-// //       }
-// //   }
-// //   assert(buf[i++] == '\n');
- 
-// //   assert(strncmp(buf + i, "HEIGHT", 6) == 0);
-// //   i += 6;
-// //   for (; i < buf_size; ++i) {
-// //       if (buf[i] == ':') { i++; break; }
-// //       assert(buf[i] == ' ');
-// //   }
-// //   for (; i < buf_size; ++i) {
-// //       if (buf[i] >= '0' && buf[i] <= '9') break;
-// //       assert(buf[i] == ' ');
-// //   }
-// //   for (; i < buf_size; ++i) {
-// //       if (buf[i] >= '0' && buf[i] <= '9') {
-// //           height = height * 10 + buf[i] - '0';
-// //       } else {
-// //           break;
-// //       }
-// //   }
- 
-// //   free(buf);
- 
-// //   screen_w = width;
-// //   screen_h = height;
-//   // if (*w == 0 && *h == 0) {
-//   //     *w = screen_w;
-//   //     *h = screen_h;
-//   //   }
-//   // canvas_w = *w;
-//   // canvas_h = *h;
-//   // canvas_x=(screen_w - canvas_w) / 2;
-//   // canvas_y=(screen_h - canvas_h) / 2;
-
-//   if (*w == 0){
-//     *w = screen_w;
-//   }else if(*w > screen_w){
-//     assert(0);
-//   }
-//   if (*h == 0){
-//     *h = screen_h;
-//   }else if(*h > screen_h){
-//     assert(0);
-//   }
-//   canvas_w = *w;
-//   canvas_h = *h;
-
-//   if (getenv("NWM_APP")) {
-//     int fbctl = 4;
-//     fbdev = 5;
-//     screen_w = *w; screen_h = *h;
-//     char buf[64];
-//     int len = sprintf(buf, "%d %d", screen_w, screen_h);
-//     // let NWM resize the window and create the frame buffer
-//     write(fbctl, buf, len);
-//     while (1) {
-//       // 3 = evtdev
-//       int nread = read(3, buf, sizeof(buf) - 1);
-//       if (nread <= 0) continue;
-//       buf[nread] = '\0';
-//       if (strcmp(buf, "mmap ok") == 0) break;
-//     }
-//     close(fbctl);
-//   }
-// }
-
-// // 向画布`(x, y)`坐标处绘制`w*h`的矩形图像, 并将该绘制区域同步到屏幕上
-// // 图像像素按行优先方式存储在`pixels`中, 每个像素用32位整数以`00RRGGBB`的方式描述颜色
-// void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
-//   int fd = open("/dev/fb", 0, 0);
-//   for (int i = 0; i < h && y + i < canvas_h; ++i) {
-//     lseek(fd, ((y + canvas_y + i) * screen_w + (x + canvas_x)) * 4, SEEK_SET);
-//     write(fd, pixels + i * w, 4 * (w < canvas_w - x ? w : canvas_w - x));
-//   }
-//   close(fd);
-// }
-
-// void NDL_OpenAudio(int freq, int channels, int samples) {
-// }
-
-// void NDL_CloseAudio() {
-// }
-
-// int NDL_PlayAudio(void *buf, int len) {
-//   return 0;
-// }
-
-// int NDL_QueryAudio() {
-//   return 0;
-// }
-
-// static void read_key_value(char *str, char *key, int* value){
-//   char buffer[128];
-//   int len = 0;
-//   for (char* c = str; *c; ++c){
-//     if(*c != ' '){
-//       buffer[len++] = *c;
-//     }
-//   }
-//   buffer[len] = '\0';
-
-//   sscanf(buffer, "%[a-zA-Z]:%d", key, value);
-//   // printf("read_key_value\n");
-// }
-
-// int NDL_Init(uint32_t flags) {
-//   if (getenv("NWM_APP")) {
-//     evtdev = 3;
-//   }
-//   char info[128], key[64];
-//   int value;
-
-//   //memset(info, 0, 128);
-//   int dispinfo = open("/proc/dispinfo", 0);
-//   read(dispinfo, info, sizeof(info));
-//   close(dispinfo);
-//   // printf("%s \n", info);
-
-//   /* 获取第一个子字符串 */
-//   char *token = strtok(info, "\n");
-   
-//    /* 继续获取其他的子字符串 */
-//    while( token != NULL ) {
-      
-//       // printf("while begin 105 %s \n", info);
-//       //printf("%s = %d\n", key, value);
-//       read_key_value(token, key, &value);
-
-//       if(strcmp(key, "WIDTH") == 0){
-//         screen_w = value;
-//       }else if(strcmp(key, "HEIGHT") == 0) {
-//         screen_h = value;
-//       }
-
-//       // printf("while middle 105 %s \n", info);
-//       token = strtok(NULL, "\n");
-//       // printf("while end 105 %s \n", info);
-//   }
-
-//   printf("With width = %d, height = %d.\n", screen_w, screen_h);
-
-//   return 0;
-// }
-
-// void NDL_Quit() {
-// }
-
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <sys/time.h>
 #include <assert.h>
+#include <fcntl.h>
 
 static int evtdev = -1;
 static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
+//画布大小
+static int canvas_w = 0, canvas_h = 0;
+//相对于屏幕左上角的画布位置坐标
+static int canvas_x = 0, canvas_y = 0;
 
 uint32_t NDL_GetTicks() {
   struct timeval tv;
   gettimeofday(&tv, NULL);
-
   return tv.tv_usec / 1000 + tv.tv_sec * 1000;
 }
 
+// 读出一条事件信息到buf中, 最长len字节
 int NDL_PollEvent(char *buf, int len) {
-  int fp = open("/dev/events", O_RDONLY);
-
-  return read(fp, buf, sizeof(char) * len);
+  int fd = open("/dev/events", 0, 0);
+  int ret = read(fd, buf, len);
+  close(fd);
+  return ret == 0 ? 0 : 1;
 }
 
-static int canvas_w, canvas_h, canvas_x = 0, canvas_y = 0;
-
+// 打开一张(*w) X (*h)的画布
+// 如果*w和*h均为0, 则将系统全屏幕作为画布, 并将*w和*h分别设为系统屏幕的大小
 void NDL_OpenCanvas(int *w, int *h) {
-  if (*w == 0){
-    *w = screen_w;
-  }else if(*w > screen_w){
-    assert(0);
-  }
-  if (*h == 0){
-    *h = screen_h;
-  }else if(*h > screen_h){
-    assert(0);
-  }
+  if (*w == 0 && *h == 0) {
+      *w = screen_w;
+      *h = screen_h;
+    }
   canvas_w = *w;
   canvas_h = *h;
+  canvas_x=(screen_w - canvas_w) / 2;
+  canvas_y=(screen_h - canvas_h) / 2;
 
   if (getenv("NWM_APP")) {
     int fbctl = 4;
@@ -270,20 +60,15 @@ void NDL_OpenCanvas(int *w, int *h) {
   }
 }
 
+// 向画布`(x, y)`坐标处绘制`w*h`的矩形图像, 并将该绘制区域同步到屏幕上
+// 图像像素按行优先方式存储在`pixels`中, 每个像素用32位整数以`00RRGGBB`的方式描述颜色
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
-  // FILE *graphics = fopen("/dev/fb", "w");
-  // for (int i = 0; i < h; ++i){
-  //   fseek(graphics, ((canvas_y + y + i) * screen_w + (canvas_x + x)) * sizeof(uint32_t), SEEK_SET);
-  //   fwrite(pixels + w * i, w * sizeof(uint32_t), 1, graphics);
-  // }
-  // fclose(graphics);
-  int graphics = open("/dev/fb", O_RDWR);
-  
-  for (int i = 0; i < h; ++i){
-    lseek(graphics, ((canvas_y + y + i) * screen_w + (canvas_x + x)) * sizeof(uint32_t), SEEK_SET);
-    ssize_t s = write(graphics, pixels + w * i, w * sizeof(uint32_t));
+  int fd = open("/dev/fb", 0, 0);
+  for (int i = 0; i < h && y + i < canvas_h; ++i) {
+    lseek(fd, ((y + canvas_y + i) * screen_w + (x + canvas_x)) * 4, SEEK_SET);
+    write(fd, pixels + i * w, 4 * (w < canvas_w - x ? w : canvas_w - x));
   }
-  //close(graphics);
+  close(fd);
 }
 
 void NDL_OpenAudio(int freq, int channels, int samples) {
@@ -300,57 +85,67 @@ int NDL_QueryAudio() {
   return 0;
 }
 
-static void read_key_value(char *str, char *key, int* value){
-  char buffer[128];
-  int len = 0;
-  for (char* c = str; *c; ++c){
-    if(*c != ' '){
-      buffer[len++] = *c;
-    }
-  }
-  buffer[len] = '\0';
-
-  sscanf(buffer, "%[a-zA-Z]:%d", key, value);
-  // printf("read_key_value\n");
-}
-
 int NDL_Init(uint32_t flags) {
+  int buf_size = 1024; 
+  char * buf = (char *)malloc(buf_size * sizeof(char));
+  int fd = open("/proc/dispinfo", 0, 0);
+  int ret = read(fd, buf, buf_size);
+  assert(ret < buf_size); // to be cautious...
+  assert(close(fd) == 0);
+ 
+  int i = 0;
+  int width = 0, height = 0;
+//使用 strncmp 函数检查字符串 "WIDTH" 是否位于 buf 中 i 处开始的位置，以确保文件内容的格式正确。
+  assert(strncmp(buf + i, "WIDTH", 5) == 0);
+  //这一行将 i 增加 5，以跳过字符串 "WIDTH"。
+  i += 5;
+  for (; i < buf_size; ++i) {
+      if (buf[i] == ':') { i++; break; }
+      assert(buf[i] == ' ');
+  }
+  for (; i < buf_size; ++i) {
+    //检查当前字符是否是数字字符。如果是，它跳出循环以开始解析宽度值。
+      if (buf[i] >= '0' && buf[i] <= '9') break;
+      assert(buf[i] == ' ');
+  }
+  for (; i < buf_size; ++i) {
+    
+      if (buf[i] >= '0' && buf[i] <= '9') {
+        //检查当前字符是否是数字字符。如果是，它将当前字符的数字值添加到 width 变量中。
+          width = width * 10 + buf[i] - '0';
+      } else {
+          break;
+      }
+  }
+  assert(buf[i++] == '\n');
+ 
+  assert(strncmp(buf + i, "HEIGHT", 6) == 0);
+  i += 6;
+  for (; i < buf_size; ++i) {
+      if (buf[i] == ':') { i++; break; }
+      assert(buf[i] == ' ');
+  }
+  for (; i < buf_size; ++i) {
+      if (buf[i] >= '0' && buf[i] <= '9') break;
+      assert(buf[i] == ' ');
+  }
+  for (; i < buf_size; ++i) {
+      if (buf[i] >= '0' && buf[i] <= '9') {
+          height = height * 10 + buf[i] - '0';
+      } else {
+          break;
+      }
+  }
+ 
+  free(buf);
+ 
+  screen_w = width;
+  screen_h = height;
+
+  
   if (getenv("NWM_APP")) {
     evtdev = 3;
   }
-
-  char info[128], key[64];
-  int value;
-
-  //memset(info, 0, 128);
-  int dispinfo = open("/proc/dispinfo", 0);
-  read(dispinfo, info, sizeof(info));
-  close(dispinfo);
-  // printf("%s \n", info);
-
-  /* 获取第一个子字符串 */
-  char *token = strtok(info, "\n");
-   
-   /* 继续获取其他的子字符串 */
-   while( token != NULL ) {
-      
-      // printf("while begin 105 %s \n", info);
-      //printf("%s = %d\n", key, value);
-      read_key_value(token, key, &value);
-
-      if(strcmp(key, "WIDTH") == 0){
-        screen_w = value;
-      }else if(strcmp(key, "HEIGHT") == 0) {
-        screen_h = value;
-      }
-
-      // printf("while middle 105 %s \n", info);
-      token = strtok(NULL, "\n");
-      // printf("while end 105 %s \n", info);
-  }
-
-  printf("With width = %d, height = %d.\n", screen_w, screen_h);
-
   return 0;
 }
 
