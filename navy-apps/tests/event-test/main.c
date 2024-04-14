816f2af93ba4e93aa3c38d501ae5afcaea43b068
tracer-ysyx <tracer@ysyx.org>
2024-04-14 21:23:12 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:23:12 up  5:07,  3 users,  load average: 0.27, 0.18, 0.15

--- navy-apps/tests/event-test/main.c
+++ navy-apps/tests/event-test/main.c
@@ -6,9 +6,7 @@ int main() {
   while (1) {
     char buf[64];
     if (NDL_PollEvent(buf, sizeof(buf))) {
-      if(strcmp(buf, "NONE") == 0){
-        printf("receive event: %s\n", buf);
-      }
+      printf("receive event: %s\n", buf);
     }
   }
   return 0;
