4f338d063450d0d6d8067992638a4e50a5e019ff
tracer-ysyx <tracer@ysyx.org>
2024-04-18 22:43:35 +0800

    > compile NEMU ysyx_22040000 周勇斐 Linux zyf-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:43:35 up 10:49,  3 users,  load average: 0.44, 0.18, 0.07

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -28,6 +28,8 @@ static int inline read_keyinfo(uint8_t *type, uint8_t *sym){
     return 0;
   }
   printf("%s\n", key_buf);
+  sscanf("%s %s\n", key_action, key_key);
+  printf("key_action=%s    key_key = %s", key_action, key_key);
   key_action = key_buf;
   int i;
   for (i = 0; key_buf[i] != ' '; i++){}
